672897761992285016d7beefc4759b1b32568199
Jeff Nuyts <NUYJE@sintcordula.be>
2025-02-19 09:19:13 +0100

    Add project files.

---
+++ Toets1H7Methoden/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Toets1H7Methoden
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declareren variabelen
+            int aantalKarakters, pincode; 
+            
+            //DEEL 1: pincode genereren
+            Console.WriteLine("##### GENEREER PINCODE #####\n");
+
+            //opvragen uit hoeveel cijfers de pincode moet bestaan
+            Console.WriteLine("Uit hoeveel cijfers moet het wachtwoord bestaan?");
+            aantalKarakters = Convert.ToInt32 (Console.ReadLine());
+
+            //pincode genereren op basis van zelf geschreven methode
+            pincode = GetPincode(aantalKarakters);
+
+            Console.WriteLine($"\nUw pincode: {pincode}\n");
+
+            //DEEL 2: aanmelden
+            Console.WriteLine("##### AANMELDEN MET PINCODE #####\n");
+
+            //pincode opvragen
+            Console.Write("Geef uw pincode op: ");
+            int pincodeTest = Convert.ToInt32 (Console.ReadLine());
+
+            Console.WriteLine();
+
+            //pincode controleren
+            if (Aanmelden(pincode, pincodeTest))
+            {
+                Console.WriteLine("U bent succesvol aangemeld.");
+            }
+            else
+            {
+                Console.WriteLine("Foutieve pincode. Toegang geweigerd.");
+            }
+
+
+        }
+
+    }
+}
